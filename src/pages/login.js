d264c0974f862b657d7d7b0cbfd15d09364bdea3
majharul <majharuljony163@gmail.com>
2023-07-27 12:49:02 +0600

    added firebase email-password auth.

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,25 @@
-import { Button } from "antd";
 import { GoogleOutlined, GithubOutlined } from "@ant-design/icons";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import Head from "next/head";
 import styles from "@/styles/Login.module.css";
+import { useForm } from "react-hook-form";
 import { signIn } from "next-auth/react";
+import auth from "@/firebase/firebase.auth";
+
 const LoginPage = () => {
+  const [signInWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    signInWithEmailAndPassword(auth, data.email, data.password);
+  };
+
+  console.log("user", user);
   return (
     <div>
       <Head>
@@ -16,12 +32,20 @@ const LoginPage = () => {
           <GithubOutlined onClick={() => signIn("github", { callbackUrl: "http://localhost:3000/" })} />
         </div>
         <hr />
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor=''>Your Email</label>
-          <input type='email' />
+          <input type='email' {...register("email", { required: true })} />
+          {errors.email && <span>This field is required</span>}
+
           <label htmlFor=''>Your Password</label>
-          <input type='password' />
-          <Button>Login</Button>
+          <input type='password' {...register("password", { required: true })} />
+          {errors.password && <span>This field is required</span>}
+
+          <br />
+
+          <button style={{ padding: "5px 0", cursor: "pointer" }} type='submit'>
+            Login
+          </button>
         </form>
       </div>
     </div>
